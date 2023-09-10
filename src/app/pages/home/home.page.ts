c7c13badd0e61ad83e492b97a2841dc3b34f24d1
Luis Rojas <lumrojasmu@unal.edu.co>
2023-09-10 17:51:24 -0500

    Ejercicio Rick and Morty

---
+++ src/app/pages/home/home.page.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { RickAndMortyService } from 'src/app/services/rick-and-morty.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+})
+export class HomePage implements OnInit {
+  characters: any[] = [];
+  params = {} as any;
+  constructor(private rymSv:RickAndMortyService) { }
+
+  ngOnInit() {
+    this.params.page = 0;
+    this.getCharacters();
+  }
+  getCharacters(event?:any) {
+    this.params.page += 1;
+    this.rymSv.getCharacters(this.params).subscribe({
+      next:(respuesta: any) => {
+        console.log(this.characters);
+        this.characters.push(...respuesta.results);
+        if(event){
+          event.target.complete();
+        }  
+      },
+      error: (err: any) => {
+        if(event){
+          event.target.complete();
+        }  
+        console.log(err)},
+    });
+  }
+}
